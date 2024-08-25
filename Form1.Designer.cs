091808fe7cdedd06efa72627302c7ab59d31558b
dgcgary <d.garcia.1@estudiantec.cr>
2024-08-24 23:21:22 -0600

    Revert "logica modificada"
    This reverts commit 6ae77857d58ab1cfcc13e3b325904d759ddc9c3d.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,8 +2,15 @@
 {
     partial class Form1
     {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -13,15 +20,20 @@
             base.Dispose(disposing);
         }
 
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.BackColor = System.Drawing.Color.Black; 
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized; 
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None; 
-            this.ResumeLayout(false);
+            this.Text = "Form1";
         }
+
+        #endregion
     }
 }
--- Grid.cs
+++ Grid.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Tron
 {
@@ -19,11 +22,11 @@ namespace Tron
             {
                 for (int y = 0; y < height; y++)
                 {
-                    nodes[x, y] = new Node { X = x * 20, Y = y * 20 };
+                    nodes[x, y] = new Node { X = x * 15, Y = y * 15 };
                 }
             }
 
-            
+            // Conectar nodos
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
@@ -38,39 +41,24 @@ namespace Tron
             }
         }
 
-        public void Draw(Graphics g, Player player)
+        public void Draw(Graphics g)
         {
-            using (Pen pen = new Pen(Color.Gray, 1)) // Establece el color de la cuadrícula a gris
+            using (Pen pen = new Pen(Color.Gray, 1))
             {
                 for (int x = 0; x < width; x++)
                 {
-                    // Draw vertical lines
-                    g.DrawLine(pen, x * 20, 0, x * 20, height * 20);
-                }
+                    for (int y = 0; y < height; y++)
+                    {
+                        Node node = nodes[x, y];
 
-                for (int y = 0; y < height; y++)
-                {
-                    // Draw horizontal lines
-                    g.DrawLine(pen, 0, y * 20, width * 20, y * 20);
-                }
-
-                // Draw player
-                using (Brush brush = new SolidBrush(Color.Red))
-                {
-                    g.FillRectangle(brush, player.X, player.Y, 20, 20);
+                        if (node.Up != null) g.DrawLine(pen, node.X, node.Y, node.Up.X, node.Up.Y);
+                        if (node.Down != null) g.DrawLine(pen, node.X, node.Y, node.Down.X, node.Down.Y);
+                        if (node.Left != null) g.DrawLine(pen, node.X, node.Y, node.Left.X, node.Left.Y);
+                        if (node.Right != null) g.DrawLine(pen, node.X, node.Y, node.Right.X, node.Right.Y);
+                    }
                 }
             }
         }
     }
 
-    public class Node
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public Node? Up { get; set; }
-        public Node? Down { get; set; }
-        public Node? Left { get; set; }
-        public Node? Right { get; set; }
-    }
 }
-
